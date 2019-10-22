bc3213a9523c4299753f74ff947552a2d62be453
JakubLem <49617551+JakubLem@users.noreply.github.com>
2019-10-23 00:20:19 +0200

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -26,10 +26,25 @@ int count()
 	file.close();
 	return counter;
 }
-void StartApp(int c)
+
+void fillzero(int* tab, int c) {
+	for (int i = 0; i < c; i++) {
+		tab[i] = 0;
+	}
+}
+int checkall(int* tab, int c) {
+	for (int i = 0; i < c; i++) {
+		if (tab[i] != 4) {
+			return 0;
+		}
+	}
+	return 1;
+}
+void StartApp(int c, int * ctab, int * wtab)
 {
+
 	fstream file;
-	int select;
+	int select,goodanswer=0,badanswer=0;
 	string word;
 	string* text = new string[c];
 	file.open("data.txt", ios::in);
@@ -41,15 +56,40 @@ void StartApp(int c)
 		srand(time(0));
 		while (true) {
 			system("cls");
+			cout << "Correct: " << goodanswer << " Wrong: " << badanswer<<endl;
 			//select the index of first word
-			select = (rand() % c/2)*2;
+			if (checkall(ctab,c) == 1) {
+				cout << "You are the best!!!";
+				goto over;
+			}
+			do
+				select = (rand() % c / 2) * 2;
+			while (ctab[select]==4);
+
 			cout <<endl<< text[select + 1] << " - ";
 			cin >> word;
 			if (word == text[select]) {
-				cout << "gratki";
+				cout << "Correct!";
+				goodanswer++;
+				ctab[select / 2]++;
+				if (ctab[select / 2] == 4) {
+					cout << endl << "COOL, you answer correct 4 times!!!";
+				}
 			}
 			else {
-				cout << "nope"<<endl<< text[select+1]<< " - " <<text[select];
+				cout << "There's sth wrong!!! "<<endl<< "Correct answet is: "<<text[select];
+				badanswer++;
+				wtab[select / 2]++;
+				if (wtab[select / 2] == 4) {
+					cout << endl << "Ughh, you hadn't translated this word fourth time! Write this 3 times correct!" << endl;
+					for (int i = 1; i < 4; i++) {
+						do
+							cin >> word;
+						while (word!=text[select]);
+						cout << endl;
+					}
+					wtab[select / 2] = 0;
+				}
 			}
 			cout << endl;
 			system("pause");
@@ -58,5 +98,6 @@ void StartApp(int c)
 	else {
 		cout << "blad";
 	}
+	over:
 	file.close();
 }
